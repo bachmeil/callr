c0cae7dcdfc5a289d2f228614a6e8f4ef62117a0
Lance Bachmeier <lance@lancebachmeier.com>
2022-12-04 21:33:00 -0600

    Trying to get gitignore to work

---
+++ lm.d
@@ -0,0 +1,43 @@
+module callr.lm;
+
+import embedr.r;
+alias rvec = RData!RVector;
+alias rlist = RData!RList;
+
+struct LM(T) {
+  RData!RVector y;
+  RData!T x;
+  bool intercept = true;
+  
+  this(RData!RVector _y, RData!T _x) {
+    y = _y;
+    x = _x;
+  } 
+
+  LMFit ols() {
+    string intTerm = "";
+    if (!intercept) {
+      intTerm = " -1";
+    }
+    auto fit = RData!RList(`lm(` ~ y.name ~ ` ~ ` ~ x.name ~ intTerm ~ `)`);
+    return LMFit(fit);
+  }
+}
+
+struct LMFit {
+  RVector coefficients;
+  RVector residuals;
+  RVector effects;
+  RVector fittedValues;
+  
+  this(RData!RList fit) {
+    coefficients = RVector(fit["coefficients"]);
+    residuals = RVector(fit["residuals"]);
+    effects = RVector(fit["effects"]);
+    fittedValues = RVector(fit["fitted.values"]);
+  }
+}
+    
+
+  
+  
